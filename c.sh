36c77bb33da7c790b0153d4ef9d7f5c58aedebc9
hanif <hanifdiyer@gmail.com>
2024-03-14 06:36:15 +0100

    clean up

--- c.sh
+++ c.sh
@@ -1,1 +1,2 @@
-g++ -g -I/usr/local/include/opencv4/ -I../src minlm.cpp ../src/*.cpp -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_calib3d -lopencv_xfeatures2d -lopencv_features2d
+g++ -g -I/usr/include/opencv4/ -I../src minlm.cpp ../src/*.cpp -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_calib3d -lopencv_xfeatures2d -lopencv_features2d -lopencv_videoio
+# g++ -I/usr/local/include/opencv4/ -I../src minlm.cpp ../src/*.cpp -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_calib3d -lopencv_xfeatures2d -lopencv_features2d 
--- minlm.cpp
+++ minlm.cpp
@@ -50,7 +50,6 @@ cv::Mat calcHomography(const std::vector<cv::Point2f>& points_map, const std::ve
     cv::eigen(A.t() * A, eVals, eVecs);
 
     cv::Mat H = cv::Mat::zeros(3, 3, CV_32F);   
-    /* cv::Mat H(3, 2, CV_32F); */   
 
     std::cout << "evecs:\n" << eVecs << "\n\n";
     std::cout << "evals:\n" << eVals << "\n\n";
@@ -67,7 +66,6 @@ cv::Mat calcHomography(const std::vector<cv::Point2f>& points_map, const std::ve
     H = H * (1.0 / H.at<float>(2, 2));
     std::cout << "6 H:\n" << H << "\n\n";
     return H;
-    /* return normalized_points_map.T.inv() * H * normalized_points_image.T; */
 }
 
 void normalizeWithSyntheticCam(const std::vector<cv::Point2f>& points, std::vector<cv::Point2f>& normalized_points){
@@ -84,8 +82,6 @@ void normalizeWithSyntheticCam(const std::vector<cv::Point2f>& points, std::vect
         
         cv::Mat ptTransformed = H.inv() * pt;
 
-        /* ptTransformed = (1.0 / ptTransformed.at<float>(2, 0)) * ptTransformed; */
-
         float newX = (ptTransformed.at<float>(0, 0));
         float newY = (ptTransformed.at<float>(1, 0));
 
@@ -95,8 +91,6 @@ void normalizeWithSyntheticCam(const std::vector<cv::Point2f>& points, std::vect
 
 void  function1_fvec(const real_1d_array &x, real_1d_array &fi, void *ptr)
 {
-
-
     for (int i=0; i<7; i++)
     {
 
@@ -137,11 +131,6 @@ void  function1_fvec(const real_1d_array &x, real_1d_array &fi, void *ptr)
         float u1 = D / (A3 * alpha + B3 * beta + C3 * gamma);
         float v1 = (A2 * alpha + B2 * beta + C2 * gamma) / (A3 * alpha + B3 * beta + C3 * gamma);
         float d = sqrt(u1 * u1 + v1 * v1) - R;
-
-        /* float u1 = D; */
-        /* float v1 = A2 * alpha + B2 * beta + C2 * gamma; */
-        /* float denominator = A3 * alpha + B3 * beta + C3 * gamma; */
-        /* float d = pow(u1, 2) + pow(v1, 2) - pow( (R * denominator), 2); */
 
         fi[i] = d;
     }
@@ -216,14 +205,8 @@ void mouse_callback(int  event, int  x, int  y, int  flag, void *param)
     }
 }
 
-/* void getPoints(const cv::Mat& input_img, std::vector<cv::Point2f>& line_img, std::vector<cv::Point2f>& circle_img) */
-/* { */
-     
-/* } */
-
 void draw_line(cv::Point pt1, cv::Point pt2, cv::Mat image, cv::Scalar line_color){
     
-       //Scalar line_Color(0, 0, 255);
        int thickness = 2;
        cv::line(image, pt1, pt2, line_color, thickness);
 }
@@ -268,34 +251,16 @@ int main(int argc, char **argv)
     normalizeWithSyntheticCam(circle_img, circle_img_norm);
     normalizeWithSyntheticCam(line_img, line_img_norm);
 
-    /* float circle_img_x[7] = {1239, 1180, 947, 712, 352, 763, 391}; */
-    /* float circle_img_y[7] = {581, 549, 672, 681, 570, 511, 646}; */
-
-    /* std::vector<cv::Point2f> circle_img; */
-    /* for (int i = 0; i < 7; ++i) */
-    /* { */
-    /*     circle_img.emplace_back(cv::Point2f(circle_img_x[i], circle_img_y[i])); */
-    /* } */
-    
-    /* normalizeWithSyntheticCam(circle_img, circle_img_norm); */
-
     float r = 9.15;
     float line_map_x[3]={0, 0, 0};
     float line_map_y[3]={-1*r, 0, 1*r};
-
-    /* float line_img_x[3]={1011, 790, 510}; */
-    /* float line_img_y[3]={521, 583, 668}; */
 
     /* std::vector<cv::Point2f> line_img; */
     std::vector<cv::Point2f> line_map;
     for(int i = 0; i < 3; ++i) 
     {
-        /* line_img.emplace_back(cv::Point2f(line_img_x[i], line_img_y[i])); */
         line_map.emplace_back(cv::Point2f(line_map_x[i], line_map_y[i]));
     }
-
-    /* std::vector<cv::Point2f> line_img_norm; */
-    /* normalizeWithSyntheticCam(line_img, line_img_norm); */
 
     cv::Mat H = calcHomography(line_map, line_img_norm);
 
@@ -306,7 +271,6 @@ int main(int argc, char **argv)
     h8=H.at<float>(2, 1);
 
     real_1d_array x = "[0.0001,0.0001,0.0001]";
-    /* real_1d_array x = "[0.0,0.0,0.0]"; */
     real_1d_array s = "[1,1,1]";
 
     double epsx = 0;
@@ -324,28 +288,16 @@ int main(int argc, char **argv)
     printf("%s\n", x.tostring(8).c_str()); 
     std::cout << "iter "<< rep.iterationscount;
 
-    /* float H_data[9] = {500, 0, 600, 0, 500, 400, 0, 0, 1}; */
-    /* cv::Mat H = cv::Mat(3, 3, CV_32F, H_data); */
-    /* H.at<float>(0, 0) = x[0]; */
-    /* H.at<float>(1, 0) = x[1]; */
-    /* H.at<float>(2, 0) = x[2]; */
-
     float alpha = x[0];
     float beta = x[1];
     float gamma = x[2];
 
     float K_data[9] = { 500, 0, 600, 0, 500, 400, 0, 0, 1 };
+
     cv::Mat K = cv::Mat(3, 3, CV_32F, K_data);
-
-    /* cv::Mat_<float> K(3,3); // rows, cols */
-    /* K << 500, 0, 600, 0, 500, 400, 0, 0, 1; */
-
     cv::Mat final_H = getH(alpha, beta, gamma, h2, h3, h5, h6, h8);
-
     cv::Mat ans = final_H * K.inv();
     cv::Mat inv_final_H = ans.inv();
-
-    /* inv_final_H = inv_final_H * (1.0 / inv_final_H.at<float>(2, 2)); */
 
     std::cout << inv_final_H << "\n";
 
